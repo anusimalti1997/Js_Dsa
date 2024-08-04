cc2d10969e8719fb9955cd4a3134c2ebdad5db52
anupamsimalti1 <anupam.simalti@appinventiv.com>
2024-08-04 08:17:21 +0530

    updates

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello');
+
+
+
+function tree(rows){
+    //it will give output
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+    counter =1;
+    for (let i = 1; i <= rows; i++) {
+       str='';
+       for(let j=1; j<=i;j++){
+            str = str + ' ' + counter
+            counter ++;
+       }
+       console.log(str);
+    }
+}
